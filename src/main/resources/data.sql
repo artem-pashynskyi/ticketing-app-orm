18cfa55e048a5fac1b1fc2bb698d0cf757a83fdc
artem-pashynskyi <mailofartem@gmail.com>
2021-01-11 16:27:14 -0500

    fix email

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,5 +6,5 @@ VALUES ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, 'Admin'),
 insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, enabled,
                   first_name, gender, last_name, user_name, role_id)
 values ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'admin', 'MALE', 'admin', 'admin@gmail.com', 1),
-       ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'John', 'MALE', 'Smith', 'john@gamil.com', 2),
-       ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'Tom', 'MALE', 'Hanks', 'tom@gamil.com', 3);
+       ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'John', 'MALE', 'Smith', 'john@gmail.com', 2),
+       ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'Tom', 'MALE', 'Hanks', 'tom@gmail.com', 3);
